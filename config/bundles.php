5e0bcfcd1b954feae0cfe50c0de9d3b350b5a0e1
Koldoshbek <koldoshbektashiev@gmail.com>
2020-04-02 09:34:50 +0300

    require vich/uploader-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Artgris\Bundle\FileManagerBundle\ArtgrisFileManagerBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
